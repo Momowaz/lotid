beff9f10e7273fec59588d811e0a22700ba06ede
Mohib <43284190+Momowaz@users.noreply.github.com>
2023-02-25 22:30:11 -0800

    Add files via upload
    add assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function (actual, expected) {
+    if (actual === expected) {
+        console.assert(actual === expected);
+        console.log("✅✅✅ Assertion Passed: ", actual, "===", expected);
+    } else {
+        console.assert(actual !== expected);
+        console.log("🛑🛑🛑 Assertion Failed: ", actual, "!===", expected);
+    }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Mohib", "Mohib");
+assertEqual(2, '2');
