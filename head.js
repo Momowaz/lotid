f4c8292097d5a51905182c9cf8ba05d3e1b87aa0
MohibHome <mohib@can-health.org>
2023-03-26 01:45:10 -0700

    added Chai and Mocha libraries for test

--- head.js
+++ head.js
@@ -1,8 +1,7 @@
 let  _ = require('lodash');
-const assertEqual = require('./assertEqual');
 
 const head = function (arr) {
     return _.head(arr);
 }
 
-module.exports = assertEqual;
+module.exports = head;
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,13 +1,6 @@
 const assertEqual = require('../assertEqual');
-const head = require('../head');
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("Mohib", "Mohib");
 assertEqual(2, '2');
-
-
-//test head.js file
-console.log('------------------- HEAD --------------------')
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
