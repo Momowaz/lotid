30707c0dcddf8a9e706c53ed3bad35bebf325647
MohibHome <mohib@can-health.org>
2023-03-26 01:54:53 -0700

    added Chai and Mocha libraries for tail test

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,15 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+assert(words.length, 3);
 
-
-
-assertEqual(tail([5, 6, 7]), [6, 7]);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
-
+describe("#tail", () => {
+    it("returns [6, 7] from [5, 6, 7]", () => {
+        assert.deepEqual(tail([5, 6, 7]), [6, 7]);
+    });
+    it("returns 'Lighthouse', 'Labs'] from ['Hello', 'Lighthouse', 'Labs']", () => {
+        assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]); 
+    });
+});
